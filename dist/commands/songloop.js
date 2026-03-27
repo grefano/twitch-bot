5f3e3113a14ab27f24aadb0a99405aaf024ceb4c
grefano <gabrielnieed@gmail.com>
2026-03-26 23:25:22 -0300

    remove dist folder

--- dist/commands/songloop.js
+++
@@ -1,4 +0,0 @@
-export default async function commandSongLoop(context) {
-    const { socket, msgId, msgUsername, msgArgs, botChannel, botOAuth, msgCommand, spotifyTokenAccess, idClient, idBroadcaster, idMod } = context;
-}
-//# sourceMappingURL=songloop.js.map
